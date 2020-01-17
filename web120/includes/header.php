3ab19596aadd89c65d352c91c854b4020a5ed6b2
erin <nire9221@gmail.com>
2020-01-16 20:07:33 -0800

    revised

--- web120/includes/header.php
+++ web120/includes/header.php
@@ -21,7 +21,7 @@
       <li><a href="index.php" class="selected">Welcome</a></li>
       <li><a href="big/index.php">Big</a></li>
       <li><a href="fp/index.php">Final Project</a></li>
-      <li><a href="contactme.php">Contact JY</a></li>
+      <li><a href="contact.php">Contact JY</a></li>
       <li class="icon"> <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a> </li>
     </ul>
   </nav>
--- web120/index.php
+++ web120/index.php
@@ -2,27 +2,12 @@
 <?php include 'includes/header.php';?>
 
 <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
-
-
-<?php
-        /*
-         * Below are 2 different forms to be re-used       
-         * 
-         * Only use one at a time, comment out the other!       
-         *
-         */
-
-        include 'includes/contact_include.php'; #site keys & code here
-    
-        $toAddress = "ecoledeflowerbiz@gmail.com";  //place your/your client's email address here
-        $toName = "ecole de flower"; //place your client's name here
-        $website = "contact form";  //place NAME of your client's website
-
-        echo loadContact('simple.php');#demonstrates a simple contact form
-        //echo loadContact('multiple.php');#demonstrates multiple form elements
-
-	?>
-    
+<img src="images/desktop.jpg" class="desktop" alt="Picture of German Shepherd" />
+ <img src="images/phone.jpg" class="phone" alt="Picture of German Shepherd" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
 </section>
 <!-- END LEFT COL -->
 
