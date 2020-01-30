ff6174b23e479930b2e9d9ffeeda71dc6dee1b03
erin <nire9221@gmail.com>
2020-01-30 13:15:52 -0800

    fix the error

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <?php include 'includes/header.php';?>
 
 <!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
-<img src="images/climbing.jpeg" class="climbing" alt="climbing"/>
- <img src="images/skating.jpeg" class="skating" alt="skating"/>
- <p>My name is Jinyoung. 
-I'm originally from South Korea.
-I've lived in Seattle for 3years 
-I worked in logistic field until 2019, 
-but a lot of my friends I met in Seattle are software engineer, and I felt it's really fun and huge achievement when i did some basic coding.  
+<img src="images/climbing.jpeg" class="desktop" alt="climbing"/>
+ <img src="images/skating.jpeg" class="phone" alt="skating"/>
+
+ <p>My name is Jinyoung. </br>
+I'm originally from South Korea.</br>
+I've lived in Seattle for 3years </br>
+I worked in logistic field until 2019,</br> 
+but a lot of my friends I met in Seattle are software engineer, </br>
+and I felt it's really fun and huge achievement when i did some basic coding. </br> 
 since that, I started exploring the job and how to be a programmer.
 </p>
 
@@ -18,11 +20,12 @@ since that, I started exploring the job and how to be a programmer.
 <!-- START RIGHT COL -->
 <aside>
  <h3>Right Column</h3>
- <img src="images/paddle.jpeg" class="paddle" alt="paddle"/>
+ <img src="images/paddle.jpeg" class="tablet" alt="paddle"/>
  <p>I really like semi-extreme sports.
 Climbing, skiing, and roller skating
 those are the most thing i do for the spare time</p>
  <p>Lorem Ipsum place holder text goes here for now</p>
+ 
 </aside>
 <!-- END RIGHT COL -->
  
